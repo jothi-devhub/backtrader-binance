edc0561238adec42f95c80d19ffe34745fe2f00e
Jothimurugan Muthusamy <131227925+jothi-devhub@users.noreply.github.com>
2023-05-22 10:20:08 -0400

    Update live_trade.py

--- examples/live_trade.py
+++ examples/live_trade.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
     store = BinanceStore(
         api_key='YOUR_BINANCE_KEY',
         api_secret='YOUR_BINANCE_SECRET',
-        coin_refer='BTC',
-        coin_target='USDT',
+        coin_refer='EUR',
+        coin_target='USD',
         testnet=True)
     broker = store.getbroker()
     cerebro.setbroker(broker)
